85b635d5055027f06d761ed35bd1cd1c9e61156d
hackexgamers <85932718+hackexgamers@users.noreply.github.com>
2024-07-21 17:06:09 +0530

    soul.py

--- soul.py
+++ soul.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7241272884:AAHwg14v4S_uSrKBXe46JRQTaKwX_nr_rJc'
+TOKEN = '6928066789:AAHyeacu69ZonQopJ6VfxzWj8HBj3Qe_TmE'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
 FORWARD_CHANNEL_ID = -1001780724402
 CHANNEL_ID = -1001780724402
