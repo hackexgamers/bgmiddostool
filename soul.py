1971d8b03fcaabfd55b4f374b8af86924e8c5599
hackexgamers <85932718+hackexgamers@users.noreply.github.com>
2024-07-24 18:49:37 +0000

    Add postStartCommand to run Python script automatically

--- soul.py
+++ soul.py
@@ -18,9 +18,9 @@ loop = asyncio.get_event_loop()
 
 TOKEN = '7241272884:AAHwg14v4S_uSrKBXe46JRQTaKwX_nr_rJc'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1001780724402
-CHANNEL_ID = -1001780724402
-error_channel_id = -1001780724402
+FORWARD_CHANNEL_ID = -1002238452878
+CHANNEL_ID = -1002238452878
+error_channel_id = -1002238452878
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
